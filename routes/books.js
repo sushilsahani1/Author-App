b0425ace63c3f0bbb0ffcf966f9aff5cf2f71ffa
Sushil Sahani <sushilsahani0012@gmail.com>
2022-10-09 07:42:30 +0530

    Very close to finish

--- routes/books.js
+++ routes/books.js
@@ -4,8 +4,27 @@ const Author = require('../models/author')
 const Book = require('../models/book')
 const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 
-router.get('/', (req , res) =>{
-    res.render("books/index")
+router.get('/', async(req , res) =>{
+    let query = Book.find()
+    if(req.query.title != null & req.query.title !== ''){
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+    if(req.query.publishedBefore != null & req.query.publishedBefore !== ''){
+        query = query.lte('publishDate', req.query.publishedBefore)
+    }
+    if(req.query.publishedAfter != null & req.query.publishedAfter !== ''){
+        query = query.gte('publishDate', req.query.publishedAfter)
+    }
+    try{
+        const books =await query.exec()
+        res.render('books/index',{
+            books : books,
+            searchOptions : req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+    
 })
 
 router.get('/new', async(req , res) =>{
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const Book = require('../models/book')
 const router  = express.Router()
 
-router.get('/', (req , res) =>{
-    res.render("main-index")
+router.get('/', async(req , res) =>{
+    let books
+    try{
+        books = await Book.find().sort({createdAt: 'desc'}).limit(6).exec()
+        res.render("main-index",{books : books})
+    } catch {
+        books = []
+    }
 })
 
 module.exports = router
